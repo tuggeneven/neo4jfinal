592a239552a7e1c4f1bec5485b5fa289151e27f0
tuggeneven <tuggeneven@outlook.com>
2022-05-02 17:11:17 -0500

    first matching

---
+++ pages/api/neo4j.js
@@ -0,0 +1,14 @@
+import neo4j from "neo4j-driver";
+
+const uri = 'neo4j+s://955946c8.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+export default async function handler(req, res) {
+    const session = driver.session();
+    const response = await_session.run( query: 'MATCH (s: Student)-[e: ENGAGED_WITH]->(m: Module) RETURN s,e,m' )
+}
+
+
