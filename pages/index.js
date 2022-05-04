c1b8fea08002533a47751a95ac83dc6f10f73a05
tuggeneven <tuggeneven@outlook.com>
2022-05-03 22:29:53 -0500

    ugh please please plesae

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head'
-
+import styles from '../styles/index.css'
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className={styles.div}>
+        <Head>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>TEAC882  Final Project</title>
+        </Head>
+
+        <header className={styles.banner}>
+            <h1 className={styles.title}>TEAC882 Final</h1>
+        </header>
+        <main className={styles.main}></main>
+
     </div>
   )
 }
