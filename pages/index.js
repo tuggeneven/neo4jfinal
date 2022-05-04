0d53968096aa392dedde3b0c3229f1bf6166dca9
tuggeneven <tuggeneven@outlook.com>
2022-05-04 18:58:40 -0500

    5.4.22

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 
 
-// const studentsApi = 'https://neo4jfinal.vercel.app/api/allstudents';
-// const modulesApi = 'https://neo4jfinal.vercel.app/api/allmodules';
-// const locationsApi = 'https://neo4jfinal.vercel.app/api/alllocations';
-//
-// fetch(studentsApi)
-//     .then(res)
+const studentsApi = 'https://neo4jfinal.vercel.app/api/allstudents';
+const modulesApi = 'https://neo4jfinal.vercel.app/api/allmodules';
+const locationsApi = 'https://neo4jfinal.vercel.app/api/alllocations';
 
+studentsBtn.document.addEventListener("click", function () {
+
+})
 
 
 export default function Home() {
@@ -17,11 +17,11 @@ export default function Home() {
             <h1>TEAC882 Final</h1>
         </header>
         <main>
-            <button className={"glow-on-hover"}> Students IDs</button>
-            <button className={"glow-on-hover"}> Modules </button>
-            <button className={"glow-on-hover"}> Locations</button>
-            <button className={"glow-on-hover"}> Average Compilation Time</button>
-            <button className={"glow-on-hover"}> Average Scores</button>
+            <button className={"glow-on-hover"} id={studentsBtn}> Students IDs</button>
+            <button className={"glow-on-hover"} id={modulesBtn}> Modules </button>
+            <button className={"glow-on-hover"} id={locationsBtn}> Locations</button>
+            <button className={"glow-on-hover"} id={avgCompTimeBtn}> Average Compilation Time</button>
+            <button className={"glow-on-hover"} id={AvgScoreBtn}> Average Scores</button>
         </main>
         <footer>
         Description:
