2a3dcde7e9b9ee2f60db3ed3565ef8a89f7d8fd8
tuggeneven <tuggeneven@outlook.com>
2022-05-03 20:37:41 -0500

    hadiiiiii

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,8 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-      <body>
-      <header>LETS GOO!</header>
-
-      <main>      </main>
-
-      <footer>      </footer>
-
-      </body>
+    <div>
+      beginning
+    </div>
   )
 }
