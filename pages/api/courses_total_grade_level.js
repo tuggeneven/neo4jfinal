710e3fa7276ead25523f15b2c9df8b0eb3ec0106
tuggeneven <tuggeneven@outlook.com>
2022-05-03 19:09:48 -0500

    First attempt

---
+++ pages/api/courses_total_grade_level.js
@@ -0,0 +1,17 @@
+import neo4j from "neo4j-driver";
+
+const uri = 'neo4j+s://955946c8.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+export default async function handler(req, res) {
+    const session = driver.session();
+    const response = await session.run ('MATCH (s: Student) RETURN s');
+
+    const records = response.records.map((record) => {
+        return record.toObject();
+    })
+    res.status(200).json({body: records});
+}
