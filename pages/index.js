3cdc7eb8ccf90cb6e7fd2f3a4cfd018c8c322be4
tuggeneven <tuggeneven@outlook.com>
2022-05-03 23:50:21 -0500

    1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-
+import '../styles/index.css'
 
 export default function Home() {
   return (
-    <div style="color: red">
+    <div>
         Lets Go!
     </div>
   )
