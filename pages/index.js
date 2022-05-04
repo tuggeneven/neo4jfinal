609be3105bc8540c309c9f2756238c71e8e6730d
tuggeneven <tuggeneven@outlook.com>
2022-05-03 22:45:45 -0500

    ugh please please plesae

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
             <title>TEAC882  Final Project</title>
         </Head>
 
-        <header className={styles.header}>
+        <header className={styles.header} style="background: #404040; color: #fff;">
             <h1 className={styles.title}>TEAC882 Final</h1>
         </header>
         <main className={styles.main}></main>
