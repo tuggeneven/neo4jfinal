e05cac28076275fc99313b02bdd11654a2c320c5
tuggeneven <tuggeneven@outlook.com>
2022-05-04 19:16:06 -0500

    5.4.22

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Head from 'next/head'
 
+let allStudentObj;
+let allModulesObj;
+let allLocationsObj;
+let avgCompTimeObj;
+let avgScoreObj;
 
-// const studentsApi = 'https://neo4jfinal.vercel.app/api/allstudents';
-// const modulesApi = 'https://neo4jfinal.vercel.app/api/allmodules';
-// const locationsApi = 'https://neo4jfinal.vercel.app/api/alllocations';
-//
-// fetch(studentsApi)
-//     .then(res)
+import neo4j from "neo4j-driver";
 
+//setting up
+const uri = 'neo4j+s://955946c8.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
 
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+async function getStudents(req, res) {
+    const session = driver.session();
+    const response = await session.run('MATCH (s:Student) RETURN s');
+
+    const records = response.records.map((record) => {
+        allStudentObj = record.toObject();
+        console.log(allStudentObj);
+    })
+}
 
 export default function Home() {
   return (
