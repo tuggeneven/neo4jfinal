811c32e072ee7b1a49c12800612de60f85f174a5
tuggeneven <tuggeneven@outlook.com>
2022-05-07 21:23:56 -0500

    5.7.22

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ const allApi = "https://neo4jfinal.vercel.app/api/allstudents";
         .then(response => response.json())
         .then(data => {
 
-            console.log(data[0])
+            console.log(data)
         });
 // }
 
