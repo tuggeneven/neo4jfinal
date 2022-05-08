c4914bc9563b4d24e69b67530e000d78b7566615
tuggeneven <tuggeneven@outlook.com>
2022-05-07 19:24:03 -0500

    5.4.22

--- pages/index.js
+++ pages/index.js
@@ -12,32 +12,32 @@ console.log('hello');
 let callBack;
 
 // setting up the API in driver
-// const uri = 'neo4j+s://955946c8.databases.neo4j.io';
-// const user = 'neo4j';
-// const password = process.env.NEO4J;
-//
-// const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
-//
-// async function studentsBtn(req, res) {
-//     const session = driver.session();
-//     const responseAll = await session.run('MATCH (n1:LOCATION)<-[r1:LIVES_IN]-(n2:Student)-[r2:ENGAGED_WITH]->(n3:Module) RETURN (n1)<-[r1]-(n2)-[r2]->(n3)');
-//
-//     const records = responseAll.records.map((record) => {
-//         callBack = record.toObject();
-//
-//     })
-// }
-//
-// console.log(callBack);
+const uri = 'neo4j+s://955946c8.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
 
-const allApi = "https://neo4jfinal.vercel.app/api/allstudents";
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+async function allData(request, response) {
+    const session = driver.session();
+    const responseAll = await session.run('MATCH (n1:LOCATION)<-[r1:LIVES_IN]-(n2:Student)-[r2:ENGAGED_WITH]->(n3:Module) RETURN (n1)<-[r1]-(n2)-[r2]->(n3)');
+
+    const records = responseAll.records.map((record) => {
+        callBack = record.toObject();
+        console.log(callBack);
+    })
+}
+
+
+
+// const allApi = "https://neo4jfinal.vercel.app/api/allstudents";
 
 //Fetching the API on page load
-let allData = function pullData () {
-    fetch(allApi)
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
+// let allData = function pullData () {
+//     fetch(allApi)
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// }
 
 
 // HTML index page
