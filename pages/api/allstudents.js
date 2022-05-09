439a75fb0a9fdaad614ac4390a3445eaa6057d02
tuggeneven <tuggeneven@outlook.com>
2022-05-09 01:25:05 -0500

    5.7.22

--- pages/api/allstudents.js
+++ pages/api/allstudents.js
@@ -9,11 +9,10 @@ const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 export default async function handler(req, res) {
     const session = driver.session();
     // const response = await session.run('MATCH (n1:LOCATION)<-[r1:LIVES_IN]-(n2:Student)-[r2:ENGAGED_WITH]->(n3:Module) RETURN (n1)<-[r1]-(n2)-[r2]->(n3)');
-    const response = await session.run('MATCH (s:Student) RETURN s');
+    const response = await session.run('MATCH (s:Student) RETURN (s)');
     const records = response.records.map((record) => {
         return record.toObject();
     })
     res.status(200).json({body: records});
 }
 
-console.log('hello');
