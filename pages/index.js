01ba24e034084a0155fc8e85bcbd7d50fffd9de5
tuggeneven <tuggeneven@outlook.com>
2022-05-09 00:34:19 -0500

    5.7.22

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import React from "react";
 import neo4j from "neo4j-driver";
 
+
 const uri = 'neo4j+s://955946c8.databases.neo4j.io';
 const user = 'neo4j';
 const password = process.env.NEO4J;
