f0cb7686cd50b2a3c6687335d2302d7255d4aaa1
tuggeneven <tuggeneven@outlook.com>
2022-05-08 22:14:40 -0500

    5.7.22

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import Head from 'next/head';
 import React from "react";
-import neo4j from "neo4j-driver";
-import { Line } from "react-chartjs-2";
-import { Doughnut } from "react-chartjs-2";
+import Content from "../pages/Components/Content";
+import Footer from '../pages/Components/Footer';
+import Header from '../pages/Components/Header';
 
-console.log('hello');
-
-
-
-// JS codes
-
-
-
-// setting up the API in driver
 const uri = 'neo4j+s://955946c8.databases.neo4j.io';
 const user = 'neo4j';
 const password = process.env.NEO4J;
@@ -32,68 +23,23 @@ async function allData(request, response) {
 
 
 
-
-// const allApi = "https://neo4jfinal.vercel.app/api/allstudents";
+// const allApi = 'https://neo4jfinal.vercel.app/api/allstudents';
 //
-// // Fetching the API on page load
 //
-//    const totalData =  fetch(allApi)
+// let studentData = fetch(allApi)
 //         .then(response => response.json())
-//         .then(data => {
-//
-//             console.log(data)
-//         });
-
-
-
-
+//         .then(json => console.log(json));
+        
 
 // HTML index page
 export default function Home() {
 
   return (
     <div>
-        <Head>
-            <title>TEAC882</title>
-            <meta name="description" content="Created by Tuggen Even" />
-        </Head>
-        <header>
-            <h1>TEAC882 Final</h1>
-            <button className="glow-on-hover">Students IDs</button>
-            <button className="glow-on-hover"> Modules </button>
-            <button className="glow-on-hover"> Locations</button>
-            <button className="glow-on-hover"> Avg. Compilation Time</button>
-            <button className="glow-on-hover"> Avg. Scores</button>
-        </header>
-
-        <main>
-            <leftcolumn className="columnOne" id="graph">
-                <div className="contentContainer">
-                    <div className="contentWrapper">
-                       <div className="charts">
-                           <div className="bar">
-                               <h2>text here</h2>
-                           </div>
-                           <div className="circle">
-                               <h2>text here</h2>
-                               {/*<Line data={totalData}> width={400} height={400}></Line>*/}
-
-                           </div>
-                       </div>
-                    </div>
-                </div>
-            </leftcolumn>
-            <rightcolumn className="columnTwo">
-
-
-
-            </rightcolumn>
-        </main>
-
-        <footer>
-
-        </footer>
+      <Head />
+      <Header />
+      <Content />                         
+      <Footer />
     </div>
   )
 }
-
