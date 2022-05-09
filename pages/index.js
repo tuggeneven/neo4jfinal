10d1035f5cb5e87600ee3bb035fc4403bcef6934
tuggeneven <tuggeneven@outlook.com>
2022-05-09 01:39:31 -0500

    5.7.22

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import Head from 'next/head';
 import React from "react";
+import neo4j from "neo4j-driver";
 
+
+const uri = 'neo4j+s://955946c8.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+async function allData(request, response) {
+    const session = driver.session();
+    //const responseAll = await session.run('MATCH (n1:LOCATION)<-[r1:LIVES_IN]-(n2:Student)-[r2:ENGAGED_WITH]->(n3:Module) RETURN (n1)<-[r1]-(n2)-[r2]->(n3)');
+    const students = await session.run('MATCH (s:student) RETURN (s)');
+    const returnedData = students.records.map((record) => {
+        let allData = record.toObject();
+        console.log("Data:", allData);
+
+    })
+}
 
 
 
@@ -13,8 +31,6 @@ import React from "react";
         
 
 // HTML index page
-
-
 export default function Home() {
 
   return (
